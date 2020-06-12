f8009b46ae411c13c368a226c1d4454fd8f44965
xyz9911 <xyz9911@163.com>
2020-06-12 11:34:30 +0800

    test

--- CampusService/settings.py
+++ CampusService/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django
+from pip._vendor.urllib3 import contrib
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
